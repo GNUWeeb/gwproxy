GNUWeeb/gwproxy

commit 8dcac056b209f5bba240a67356c530065e4c558b

dns_resolver: Add DNS resolution interface APIs
Provide an interface for DNS resolution to be used by other components
without including the socket handling code. It allows other components
to handle the socket I/O as they see fit, flexibly. For instance, using
epoll vs using io_uring has different ways of handling the socket I/O.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20260713111338.3841159-6-alviro.iskandar@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/gwproxy/dns_resolver.c
@@ -250,3 +250,128 @@ void gwp_dns_res_free(struct gwp_dns_resolver *res)
 	__sys_close(res->udp_fd);
 	memset(res, 0, sizeof(*res));
 }
+
+int gwp_dns_res_prep_query(struct gwp_dns_resolver *res,
+			   struct gwp_dns_packet *gdp)
+{
+	int af, ret;
+	ssize_t tmp;
+
+	assert(gdp->gcp);
+
+	if (!gdp->__in_fallback_attempt) {
+		ret = insert_map(res->sess_map, gdp->gcp, &gdp->txid);
+		if (ret)
+			return ret;
+	} else {
+		assert(gdp->gcp == lookup_map(res->sess_map, gdp->txid));
+	}
+
+	ret = 0;
+	if (gdp->restyp == GWP_DNS_RESTYP_PREFER_IPV6) {
+		af = AF_INET6;
+		gdp->restyp = GWP_DNS_RESTYP_IPV4_ONLY;
+		gdp->__in_fallback_attempt = true;
+	} else if (gdp->restyp == GWP_DNS_RESTYP_PREFER_IPV4) {
+		af = AF_INET;
+		gdp->restyp = GWP_DNS_RESTYP_IPV6_ONLY;
+		gdp->__in_fallback_attempt = true;
+	} else if (gdp->restyp == GWP_DNS_RESTYP_IPV6_ONLY) {
+		ret = 0;
+		af = AF_INET6;
+	} else if (gdp->restyp == GWP_DNS_RESTYP_IPV4_ONLY) {
+		ret = 0;
+		af = AF_INET;
+	} else {
+		ret = -EINVAL;
+		goto err;
+	}
+
+	tmp = gwdns_build_query(gdp->txid, gdp->host, af, gdp->buf, gdp->buf_len);
+	if (tmp < 0) {
+		ret = (int)tmp;
+		goto err;
+	}
+
+	gdp->buf_len = (uint16_t)tmp;
+	return ret;
+
+err:
+	delete_map(res->sess_map, gdp->txid);
+	return ret;
+}
+
+void gwp_dns_res_drop_query(struct gwp_dns_resolver *res,
+			    struct gwp_conn_pair *gcp, uint16_t txid)
+{
+	assert(gcp);
+	assert(gcp == lookup_map(res->sess_map, txid));
+	delete_map(res->sess_map, txid);
+	(void)gcp;
+}
+
+int gwp_dns_res_fetch_gcp_by_payload(struct gwp_dns_resolver *res,
+				     const uint8_t buf[UDP_MSG_LIMIT],
+				     uint16_t len,
+				     struct gwp_conn_pair **gcp_p)
+{
+	uint16_t txid;
+
+	if (len < 38)
+		return -EINVAL;
+
+	memcpy(&txid, buf, 2);
+	if (txid >= res->sess_map->cap)
+		return -EINVAL;
+
+	*gcp_p = lookup_map(res->sess_map, txid);
+	if (!gcp_p)
+		return -ENOENT;
+
+	return 0;
+}
+
+int gwp_dns_res_complete_query(struct gwp_dns_resolver *res,
+				struct gwp_dns_packet *gdp,
+				uint8_t buf[UDP_MSG_LIMIT],
+				uint16_t len,
+				struct gwp_sockaddr *addr)
+{
+	struct gwdns_addrinfo_node *ai = NULL;
+	char port[sizeof("65535")];
+	uint16_t txid;
+	ssize_t r;
+
+	if (len < 38)
+		return -EINVAL;
+
+	memcpy(&txid, buf, 2);
+	if (txid >= res->sess_map->cap)
+		return -EINVAL;
+
+	if (gdp->txid != txid)
+		return -EINVAL;
+
+	if (gdp->gcp != res->sess_map->sess_map[txid])
+		return -EINVAL;
+
+	snprintf(port, sizeof(port), "%hu", gdp->port);
+	r = gwdns_parse_query(txid, port, buf, len, &ai);
+	if (!r) {
+		*addr = ai->ai_addr;
+		gwdns_free_parsed_query(ai);
+		gwp_dns_res_drop_query(res, gdp->gcp, txid);
+		return 0;
+	}
+
+	if (gdp->__in_fallback_attempt) {
+		int tmp = gwp_dns_res_prep_query(res, gdp);
+		if (!tmp)
+			return -EAGAIN;
+
+		r = tmp;
+	}
+
+	gwp_dns_res_drop_query(res, gdp->gcp, txid);
+	return (int)r;
+}

src/gwproxy/dns_resolver.h
@@ -1,7 +1,9 @@
 #ifndef GWPROXY__DNS_RESOLVER_H
 #define GWPROXY__DNS_RESOLVER_H
 
+#include <gwproxy/dns_parser.h>
 #include <gwproxy/gwproxy.h>
+#include <gwproxy/dns.h>
 
 struct gwp_ctx;
 struct gwp_dns_resolver_map;
@@ -12,8 +14,36 @@ struct gwp_dns_resolver {
 	struct gwp_dns_resolver_map	*sess_map;
 };
 
+struct gwp_dns_packet {
+	bool			__in_fallback_attempt;
+	uint8_t			restyp;
+	uint16_t		txid;
+	uint16_t		buf_len;
+	uint16_t		port;
+	uint8_t			buf[UDP_MSG_LIMIT];
+	char			*host;
+	struct gwp_conn_pair	*gcp;
+};
+
 int gwp_dns_res_init(struct gwp_ctx *ctx, struct gwp_dns_resolver *gdr,
 		     const char *srv_addr);
 void gwp_dns_res_free(struct gwp_dns_resolver *gdr);
+
+int gwp_dns_res_prep_query(struct gwp_dns_resolver *res,
+			   struct gwp_dns_packet *gdp);
+
+void gwp_dns_res_drop_query(struct gwp_dns_resolver *res,
+			    struct gwp_conn_pair *gcp, uint16_t txid);
+
+int gwp_dns_res_fetch_gcp_by_payload(struct gwp_dns_resolver *res,
+				     const uint8_t buf[UDP_MSG_LIMIT],
+				     uint16_t len,
+				     struct gwp_conn_pair **gcp_p);
+
+int gwp_dns_res_complete_query(struct gwp_dns_resolver *res,
+				struct gwp_dns_packet *gdp,
+				uint8_t buf[UDP_MSG_LIMIT],
+				uint16_t len,
+				struct gwp_sockaddr *addr);
 
 #endif /* #ifndef GWPROXY__DNS_RESOLVER_H */

commit 7d55f717b07615fd0cb2ce37b3149fe88bfe84b5

dns_resolver: Fix NULL check in gwp_dns_res_fetch_gcp_by_payload
The function sets *gcp_p to the result of lookup_map(), which returns
NULL when no session matches the txid (a duplicate/late reply, or a
reply whose slot is already empty). The guard tested `!gcp_p` -- the
address of the caller's out-parameter, which is never NULL -- instead
of `!*gcp_p`, so it returned success with *gcp_p == NULL. The raw-DNS
epoll handler then dereferences that NULL gcp and crashes the whole
process.

Test the looked-up pointer instead.

Fixes: 8dcac056b209 ("dns_resolver: Add DNS resolution interface APIs")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

src/gwproxy/dns_resolver.c
@@ -325,7 +325,7 @@ int gwp_dns_res_fetch_gcp_by_payload(struct gwp_dns_resolver *res,
 		return -EINVAL;
 
 	*gcp_p = lookup_map(res->sess_map, txid);
-	if (!gcp_p)
+	if (!*gcp_p)
 		return -ENOENT;
 
 	return 0;